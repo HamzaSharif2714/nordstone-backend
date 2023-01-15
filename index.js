c0a2e26d357857123e6ed5c3d262bee5dadc5a16
HamzaSharif2714 <hamzasaeed.2714@gmai.com>
2023-01-15 23:20:30 +0500

    Final Update

--- index.js
+++ index.js
@@ -1,27 +1,8 @@
 const express = require("express");
-const mongoose = require("mongoose");
-const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 3000;
-mongoose.set("strictQuery", false);
-// Connect to MongoDB
-mongoose.connect("mongodb://nordstone.up.railway.app", {
-  useNewUrlParser: true,
-});
-app.use(cors());
+
 app.use(express.json());
-
-// Create a schema for the calculation results
-const calculationSchema = new mongoose.Schema({
-  operation: String,
-  num1: Number,
-  num2: Number,
-  result: Number,
-  timestamp: { type: Date, default: Date.now },
-});
-
-// Create a model for the calculation results
-const Calculation = mongoose.model("Calculation", calculationSchema);
 
 app.post("/calculate", (req, res) => {
   const { num1, num2, operation } = req.body;
@@ -39,26 +20,10 @@ app.post("/calculate", (req, res) => {
     default:
       return res.status(400).send({ error: "Invalid operation" });
   }
-  // Create a new calculation object
-  const calculation = new Calculation({
-    operation,
-    num1,
-    num2,
-    result,
-  });
-  // Save the calculation object to the database
-  calculation.save((err) => {
-    if (err) {
-      res.status(500).send(err);
-    } else {
-      res.json({ result });
-    }
-  });
-});
-app.get("/", (req, res) => {
-  res.send("Hello");
+
+  res.json({ result });
 });
 
 app.listen(port, () => {
-  console.log("Server listening on port 3000");
+  console.log(`Server listening on port ${port}`);
 });
